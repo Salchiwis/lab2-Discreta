f35bf7f135a4d21716c49d3e7358239547fac760
Salchi <francisco.porceldeperalta@mi.unc.edu.ar>
2024-04-29 06:20:36 -0300

    3.1 y 3.2 sin Sort
    todas las estructuras y funciones auxiliares necesarias para los puntos 3.1 y 3.2
    Faltaria el Greedy e implementar los sortings para el 3, con "Milanesa" esta donde hay que implementarlos y abajo de todo comentarios con como va el asunto y como habria que hacer el sort

--- API2024parte2.c
+++ API2024parte2.c
@@ -1,5 +1,6 @@
 #include "API2024parte2.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 //Greedy --> Complexity --> O( n * (d+d+d)) --> O(n * 3d)
 // --> O(3n*d) --> O(3*2m) --> O(6m) --> O(m)
@@ -21,7 +22,7 @@ u32 Greedy(Grafo G, u32 * Orden){
         for (u32 j = 0; j < how_many_vecinos; j++)
         {
             u32 vert_vecino = Vecino(j,vert_selected,G);
-          }
+        }
     }
 
     if (vert_painted != nv)
@@ -32,3 +33,245 @@ u32 Greedy(Grafo G, u32 * Orden){
     return total_colors;
 
 }
+
+//Estructuras y Funciones Auxiliares para guardar la Informacion de los Grafos al Ordenarlos
+//Estructura con una lista de Vertices de Determinado Color y su Tamaño y sus M y m.
+typedef struct setSameColor {
+    u32 *vertices;
+    u32 lenVertices;
+    u32 color;
+    u32 mGrande;
+    u32 mChica;
+    bool añadido;
+    u32 size; //Tamaño Allocado para el Array
+} setSetColor;
+
+//Funcion para Inicializar el Arreglo de Vertices
+void initArray(setSameColor *a, u32 initialSize) {
+  a->vertices = malloc(initialSize * sizeof(u32));
+  a->lenVertices = 0;
+  a->color = 0
+  a->añadido = false;
+  a->mGrande = 0;
+  a->mChica = 0;
+  a->size = initialSize;
+}
+
+//Funcion para Insertar en el Arreglo de Vertices
+void insertArray(setSameColor *a, u32 element) {
+   if (a->lenVertices == a->size) {
+    a->size *= 2;
+    a->vertices = realloc(a->vertices, a->size * sizeof(u32));
+  }
+  a->vertices[a->lenVertices++] = element;
+}
+
+//Funcion para Liberar el Arreglo de Vertices
+void freeArray(setSameColor *a) {
+  free(a->vertices);
+  a->vertices = NULL;
+  a->lenVertices = a->size = 0;
+}
+
+char GulDukat(Grafo G,u32* Orden){
+    //Creamos la lista con la Maxima cantidad de Colores Posibles.
+    u32 delta = Delta(G)+1
+    u32 nv = NumeroDeVertices(G);
+    color maxColor = 0;
+    setSameColor lista [delta];
+    //O n
+    //Seria mejor si ya supieramos "r" (Cuantos colores son)
+    for (u32 i = 0; i < delta; i++) {
+        initArray(&lista[i], 1)
+    }
+
+    //O n 
+    //Llenar la Estructura
+    for (u32 i = 0; i < nv; i++)
+    {
+        u32 vert_selected = Orden[i];
+        color col = Color(vert_selected, G) 
+        if (col > maxColor){ maxColor = color }
+        if (col == 0)
+        {
+            //Grafo no Coloreado Previamente
+            return '1';
+        }
+        insertArray(&lista[col-1], vert_selected);
+        lista[col-1].color = col;
+        u32 grad = Grado(vert_selected, G)
+
+        if (lista[col-1].mGrande < grad)
+        {lista[col-1].mGrande = grad;}
+
+        if (lista[col-1].mChica > grad)
+        {lista[col-1].mChica = grad;}
+    }
+
+
+    //Ordeno la lista por disibile por 4(M) -> Par(M+m) -> Impar(m) MILANESA
+
+    //Si te pones a pensar solo pasa por las partes de añadir 1 vez por vertice asi que queda
+    //O n
+    //No hay necesidad de ver el arreglo pasado el maximo de colores utilizado
+    //Iteramos la Lista ya Ordenada y añadimos
+    u32 va = 0 //Cantidad de Vertices Agregados a Orden
+    for (u32 i = 0; i < maxColor; i++)
+    {
+        if(lista[i].añadido == false)
+        {
+            for (u32 j = va; j < (va + lista[i].lenVertices); j++)
+            {
+                va++;
+                Orden[j] = lista[i].vertices[j - va];
+                lista[i].añadido = true;
+            }
+        }
+    }
+
+    //O n
+    //Liberamos la memoria de las Estructuras Auxiliares
+    for (u32 i = 0; i < delta; i++) {
+        freeArray(&lista[i])
+    }
+
+    //Chequeamos Errores y Finalizamos
+    if(va != nv){return '1'}
+    return '0';
+}
+
+char ElimGarak(Grafo G, u32 *Orden){
+    //Creamos la lista con la Maxima cantidad de Colores Posibles.
+    u32 delta = Delta(G)+1
+    u32 nv = NumeroDeVertices(G);
+    color maxColor = 0;
+    setSameColor lista [delta];
+    //O n
+    //Seria mejor si ya supieramos "r" (Cuantos colores son)
+    for (u32 i = 0; i < delta; i++) {
+        initArray(&lista[i], 1)
+    }
+
+    //O n 
+    //Llenar la Estructura
+    for (u32 i = 0; i < nv; i++)
+    {
+        u32 vert_selected = Orden[i];
+        color col = Color(vert_selected, G) 
+        if (col > maxColor){ maxColor = color }
+        if (col == 0)
+        {
+            //Grafo no Coloreado Previamente
+            return '1';
+        }
+        insertArray(&lista[col-1], vert_selected);
+        lista[col-1].color = col;
+    }
+
+
+    //Ordenar Lista por Lista[i].lenvertices de menor a mayor MILANESA
+
+    //Si te pones a pensar solo pasa por las partes de añadir 1 vez por vertice asi que queda
+    //O n
+    //No hay necesidad de ver el arreglo pasado el maximo de colores utilizado
+    //Los colores 1 y 2 se agregan aparte (Indices 0 y 1)
+    //Iteramos la Lista ya Ordenada y añadimos
+    u32 va = 0 //Cantidad de Vertices Agregados a Orden
+    u32 c1 = 0 //Indice del Color 1
+    u32 c2 = 0 //Indice del Color 2
+    for (u32 i = 0; i < maxColor; i++)
+    {
+        if(lista[i].color == 1){c1 = i; continue;}
+        if(lista[i].color == 2){c2 = i; continue;}
+        if(lista[i].añadido == false)
+        {
+            for (u32 j = va; j < (va + lista[i].lenVertices); j++)
+            {
+                va++;
+                Orden[j] = lista[i].vertices[j - va];
+                lista[i].añadido = true;
+            }
+        }
+    }
+
+    //Añadir c2
+    if(lista[c2].añadido == false && maxColor > 1)
+    {
+        for (u32 j = va; j < (va + lista[c2].lenVertices); j++)
+        {
+            va++;
+            Orden[j] = lista[c2].vertices[j - va];
+            lista[c2].añadido = true;
+        }
+    }
+
+    //Añadir c1
+    if(lista[c1].añadido == false)
+    {
+        for (u32 j = va; j < (va + lista[c1].lenVertices); j++)
+        {
+            va++;
+            Orden[j] = lista[c1].vertices[j - va];
+            lista[c1].añadido = true;
+        }
+    }
+
+    //O n
+    //Liberamos la memoria de las Estructuras Auxiliares
+    for (u32 i = 0; i < delta; i++) {
+        freeArray(&lista[i])
+    }
+
+    //Chequeamos Errores y Finalizamos
+    if(va != nv){return '1'}
+    return '0';
+}
+
+    /*
+    To do List para ElimGarak (* significa hecho; - significa no hecho)
+   * Creamos un Array de SetsDeVerticesSameColor de Delta + 1 de tamaño. O 1
+   * Iteramos en los Vertices del Array y los agregamos donde corresponda en la Estructura Anterior (Index = Color - 1)
+   * Ademas llevamos la cuenta de cual es el Color mas Alto usado hasta entonces. O n
+   * SetsDeVerticesSameColor: con Color, Lista de Vertices, Cardinal de esa Lista y un Bool de Añadido
+
+   - Ordeno la lista por lenvertices MILANESA
+
+   * añado a los que no sean el 1 o el 2 llevando la cuenta de cuantos ya añadi. O n
+   * cuando no queden mas añado el 2 y el 1 y termina. O n
+
+   * Libero la memoria de las estructuras auxiliares O n
+
+   de momento va O 3n creo, pero falta el sort por lo qeu quiezas queda nlogn
+    */
+
+    /*
+    To do List para GulDukat (* significa hecho; - significa no hecho)
+   * Creamos un Array de SetsDeVerticesSameColor de Delta + 1 de tamaño. O 1
+   * Iteramos en los Vertices del Array y los agregamos donde corresponda en la Estructura Anterior (Index = Color - 1)
+   * Ademas llevamos la cuenta de cual es el Color mas Alto usado hasta entonces. O n
+   * SetsDeVerticesSameColor: con Color, Lista de Vertices, Cardinal de esa Lista y un Bool de Añadido y M y n 
+
+   - Ordeno la lista por disibile por 4(M) -> Par(M+m) -> Impar(m) MILANESA
+
+   - Añado a los colores divisibles por 4, ordenados por M, luego a los pares por M + m y finalmente a los impares por m. O -
+
+   * Libero la memoria de las estructuras auxiliares O n
+
+   de momento va O 3n creo, pero falta el sort por lo qeu quiezas queda nlogn
+    */
+
+    /*
+    Faltaria Implementar un sort (Preferiblemente en O n) y los requisitos de comparacion, 
+    estaria bueno que se pudiera usar el mismo sort y pasarle cual condicion usar.
+    ya sea que tome un booleano de argumento o de alguna forma para no tener que repetir codigo, 
+    habria que ver cual algoritmo de ordenacion es mas rapido para este caso en particular. 
+    
+    para ElimGarak ">" seria "lista[i].lenvertices > lista[j].lenvertices"
+
+    en cambio para GulDukat es mas complicado, ">" seria algo como
+    "  if lista[i].color div 4 y lista[j].color no, entonces la primera va primero"
+    "elif lista[i].color div 4 y lista[j].color tambien, se ordenan por lista[x].mGrande"
+    "elif lista[i].color div 2 y lista[j].color no, entonces la primera va primero"
+    "elif lista[i].color div 2 y lista[j].color tambien, se ordenan por lista[x].mGrande + lista[x].mChica"
+    "elif lista[i].color no div  y lista[j].color tampoco, se ordenan por lista[x].mChica"
+    */
