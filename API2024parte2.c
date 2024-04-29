d2764243d48726b001d218d67be76bb8421bba8b
RenzoSC <renzoc009@gmail.com>
2024-04-29 01:50:00 -0300

    Update API2024parte2.c

--- API2024parte2.c
+++ API2024parte2.c
@@ -1,10 +1,14 @@
-
 #include "API2024parte2.h"
+#include <stdio.h>
 
+//Greedy --> Complexity --> O( n * (d+d+d)) --> O(n * 3d)
+// --> O(3n*d) --> O(3*2m) --> O(6m) --> O(m)
+// Aclaración --> n*d = 2m por el lema de apreton de manos 
 u32 Greedy(Grafo G, u32 * Orden){
     u32 nv = NumeroDeVertices(G);
     u32 vert_painted =0;
-    u32 used_colors =0;
+    u32 total_colors =0;
+    u32 delta = Delta(G)+1;
     for (u32 i = 0; i < nv; i++)
     {
         u32 vert_selected = Orden[i];
@@ -13,14 +17,11 @@ u32 Greedy(Grafo G, u32 * Orden){
             return INT32_MAX;
         }
         u32 how_many_vecinos = Grado(vert_selected, G);
-        color used_colors[how_many_vecinos];
+        color used_colors[delta];
         for (u32 j = 0; j < how_many_vecinos; j++)
         {
             u32 vert_vecino = Vecino(j,vert_selected,G);
-            used_colors[j] = Color(vert_vecino,G);
-        }
-
-        
+          }
     }
 
     if (vert_painted != nv)
@@ -28,6 +29,6 @@ u32 Greedy(Grafo G, u32 * Orden){
         return INT32_MAX;
     }
 
-    return
+    return total_colors;
 
 }
